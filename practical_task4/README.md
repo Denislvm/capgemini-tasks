52c8219cd7413c23e35c498ae2e3c3d936953f50
Denislvm <slyusarenkodenisss@gmail.com>
2025-02-03 01:19:51 +0200

    added some new files

--- practical_task4/README.md
+++ practical_task4/README.md
@@ -4,3 +4,8 @@
 ![image info](pict/1.1.jpg)
 ![image info](pict/1.2.jpg)
 ![image info](pict/1.3.jpg)
+
+#### Practical Task 2: Configure Environment Variables in ACI via Azure Portal
+![image info](pict/2.2.png)
+![image info](pict/2.1.png)
+
--- practical_task4/flask_app/app.py
+++ practical_task4/flask_app/app.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return "Hello Azure World!"
+# Read values from environment variables
+APP_MESSAGE = os.getenv("APP_MESSAGE", "Default Message")
+PORT = int(os.getenv("PORT", 8000))
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+@app.route("/")
+def home():
+    return f"Message: {APP_MESSAGE}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT)
